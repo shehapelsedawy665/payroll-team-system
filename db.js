3f20260215a3da2a6337ecc04418fda62755f72f
shehapelsedawy665 <shehapelsedawy666@gmail.com>
2026-04-05 08:07:27 +0200

    Update db.js

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
     if (mongoose.connection.readyState >= 1) return;
     try {
         await mongoose.connect("mongodb://Sedawy:Shehapelsedawy%2366@ac-uso95cd-shard-00-00.a6bquen.mongodb.net:27017,ac-uso95cd-shard-00-01.a6bquen.mongodb.net:27017,ac-uso95cd-shard-00-02.a6bquen.mongodb.net:27017/payrollDB?ssl=true&replicaSet=atlas-129j51-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Egyptian-Payroll");
-        console.log("✅ Database Connected Successfully");
+        console.log("✅ MongoDB Ready");
     } catch (err) {
-        console.error("❌ MongoDB Connection Error:", err);
+        console.error("❌ Connection Error:", err);
     }
 };
 
