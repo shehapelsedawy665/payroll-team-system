36abc827f52b52757655ea728ff045e4d3cd156d
shehapelsedawy665 <shehapelsedawy666@gmail.com>
2026-04-11 16:36:48 +0200

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+// backend/models/User.js
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'hr', 'employee'], default: 'employee' },
+    employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee' }, // ربط اليوزر بملف الموظف
+    companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+    status: { type: String, default: 'active' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
